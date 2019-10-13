6ed331d6598578de3483525af4b40df39bfb9670
vmanav <zmanav.1999@gmail.com>
2019-10-13 12:41:48 +0530

    CN project, added basics communication setup without server in between

--- project/server.js
+++ project/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const ExpressPeerServer = require('peer').ExpressPeerServer;
 
+users = [];
+
+app.set('view engine', 'hbs');
 const PORT = 3000;
 const server = app.listen(PORT, () => {
     console.log("Running on : http://localhost:3000")
@@ -13,4 +16,17 @@ const options = {
 
 const peerserver = ExpressPeerServer(server, options);
 
-app.use('/p2p', peerserver);
+app.use('/p2pServer', peerserver);
+
+app.get('/p2p', (req, res) => {
+    res.render('temp.hbs', { users })
+})
+
+peerserver.on('connection', (client) => {
+    console.log("A client has been connected")
+    console.log(client)
+    users.push(client)
+    console.log("Users Available : ", users)
+    
+});
+
