53b399d565b028f815158a32c071db274bd567e5
vmanav <zmanav.1999@gmail.com>
2019-11-19 23:14:05 +0530

    added final code from PeerChat Application

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -4,9 +4,7 @@ socket.on('connected', () => {
     console.log("Socket ID :", socket.id)
 })
 
-
 // old code ->
-
 
 // $(() => {
 
@@ -220,10 +218,251 @@ socket.on('connected', () => {
 // })
 
 
-// New Code from PeerChatApplication ->
+// // Revision-1 from PeerChatApplication ->
+
+// $(() => {
+
+//     // alert("LOCKED and LOADED !")
+
+//     let myId = $('#myId');
+//     let connectButtton = $('#connectButtton');
+//     let chatWindow = $('#chatWindow');
+//     let send = $('#send');
+//     let msg = $('#msg');
+//     let destPeerID = $('#destPeerID');
+//     let chatList = $('#chatList');
+//     let loginButton = $('#loginButton');
+//     let username = $('#username');
+//     let afterLogIn = $('#afterLogIn');
+//     let loginDiv = $('#loginDiv');
+//     let userList = $('#userList');
+//     let myUsername = $('#myUsername');
+//     let vChatButtton = $('#vChatButtton');
+//     let vChatWindow = $('#vChatWindow');
+//     let vChatend = $('#vChatend');
 
 
 
+//     // // Creating a new Peer for local machine
+//     // var peer = new Peer({
+//     //     host: location.hostname,
+//     //     port: 3000,
+//     //     path: '/p2pServer',
+//     // });
+
+
+//     // configuring PeerJSServer on Heroku
+//     // Creating a new Peer
+
+//     var peer = new Peer({
+//         secure: true,
+//         host: 'the-peer-chat.herokuapp.com',
+//         port: 443,
+//         path: '/p2pServer',
+//     });
+
+
+//     // When a new Peer is Created
+//     peer.on('open', function (id) {
+//         console.log('My peer ID is: ' + id);
+//         myId.text(id)
+
+//     });
+
+//     loginButton.click(() => {
+
+//         let usernameValue = username.val();
+//         if (usernameValue == "") {
+//             alert("Please Provide a username.")
+//             return;
+//         }
+//         console.log("user ->", usernameValue)
+//         myUsername.text(usernameValue)
+
+//         loginDiv.removeClass("d-flex").addClass("d-none");
+//         afterLogIn.show("fast");
+
+//         // Only if a user allows!
+//         socket.emit('addUser', {
+//             username: usernameValue,
+//             peerId: myId.text()
+//         })
+
+//         socket.on('alertAllAboutNewUser', (data) => {
+//             console.log("enter alert all `on` event")
+//             console.log("Data is -->", data);
+//             console.log(data.list)
+//             refreshUserList(data.list)
+//         })
+
+//         // peer.on('disconnected', function () {
+//         // });
+
+
+//     })
+
+//     // Error Handling
+//     peer.on('error', function (err) {
+//         console.log("ERROR ->", err);
+//         console.log("Error Type: ", err.type);
+//         alert("Error Encountered : " + err.type);
+//     })
+
+
+//     // Recieving a connection
+//     peer.on('connection', function (connectionObject) {
+//         // console.log("someone tried conecteing")
+//         console.log("connectionObject", connectionObject)
+//         console.log("Peer ID: " + connectionObject.peer + " , tried Connectiong You.")
+
+//         connectionObject.on('open', function () {
+
+//             // Receive Messages
+//             connectionObject.on('data', function (data) {
+//                 // console.log('Received', data);
+//                 chatList.append(`<li>${data}</li>`);
+//             });
+
+//         });
+
+//     });
+
+
+//     // Clikcing on the Connect Button
+//     connectButtton.click(() => {
+
+//         destPeerIDValue = destPeerID.val();
+//         if (destPeerIDValue == "") {
+//             alert("Please Provide a PeerID.")
+//             return;
+//         }
+//         var conn = peer.connect(destPeerIDValue);
+//         if (!conn) {
+//             alert("Please Provide a VALID PeerID.");
+//             return
+//         }
+//         conn.on('open', function () {
+
+//             // Send Messages
+//             send.click(() => {
+//                 let textToSend = msg.val();
+
+//                 if (textToSend == "") {
+//                     // EMPTY MSSG BODY
+//                     alert("Why send an empty Message ?")
+//                     return
+//                 }
+
+//                 updatedTextToSend = myUsername.text() + " : " + textToSend;
+
+//                 // send to other users
+//                 conn.send(updatedTextToSend);
+
+//                 // Also add at your screen
+//                 chatList.append(`<li style="text-align: right;">${textToSend}</li>`);
+
+//                 msg.val("");
+//             })
+//         });
+
+//     });
+
+
+//     // Recieving a call
+//     peer.on('call', function (call) {
+//         // Answer the call, providing our mediaStream
+//         console.log("inside Recieving Call ")
+
+//         // make videoChat visible
+//         vChatWindow.show();
+//         call.answer();
+
+//         // Ending video Call from Reciver side
+//         vChatend.click(() => {
+//             console.log("end Chat clickedddd");
+//             call.close();
+//         })
+
+//         call.on('close', function () {
+//             console.log("in call.on(close)");
+//             var video = document.getElementById('vChat')
+//             video.srcObject = null;
+
+//         })
+
+//         call.on('stream', function (stream) {
+//             // `stream` is the MediaStream of the remote peer.
+//             // Here you'd add it to an HTML video/canvas element.
+//             console.log("inside on 'stream'---")
+
+//             // attach 
+//             var video = document.getElementById('vChat')
+//             video.srcObject = stream;
+//             video.play()
+
+//         });
+
+//     });
+
+
+//     vChatButtton.click(() => {
+//         console.log("Inside VchatButton.Click()");
+//         // make videoChat visible
+//         vChatWindow.show();
+
+//         navigator.webkitGetUserMedia({
+//             video: true,
+//             audio: true
+//         }, function (stream) {
+
+//             destPeerIDValue = destPeerID.val();
+//             if (destPeerIDValue == "") {
+//                 alert("Please Provide a PeerID.")
+//                 vChatWindow.hide();
+//                 return;
+//             }
+//             var call = peer.call(destPeerIDValue, stream);
+
+//             if (!call) {
+//                 alert("Please Provide a VALID PeerID.");
+//                 vChatWindow.hide();
+//                 return
+//             }
+
+//             // attach your own stream
+//             var video = document.getElementById('vChatMyStream')
+//             video.srcObject = stream;
+//             video.play()
+
+
+//             vChatend.click(() => {
+//                 console.log("end Chat milane vale ke liye clickedddd");
+//                 call.close();
+//                 var video = document.getElementById('vChatMyStream')
+//                 video.srcObject = null;
+
+//             })
+
+//         }, function (err) {
+//             console.log("Error ->", err)
+//             // console.log(err)
+//         })
+//     })
+
+//     function refreshUserList(listOfUsers) {
+//         userList.text("")
+//         listOfUsers.forEach(myFunction);
+//         function myFunction(item) {
+//             // console.log(item)
+//             userList.append(`<li><b>${item.username}</b> : <u><i>${item.peerId}</i></u></li>`)
+//         }
+//     }
+
+// })
+
+
+
+// Revision-2 from PeerChatApplication ->
 
 $(() => {
 
@@ -247,6 +486,9 @@ $(() => {
     let vChatend = $('#vChatend');
 
 
+    // For Connecting to peerJS Serevr rather than our nodejs hosted server
+    var peer = new Peer({
+    });
 
     // // Creating a new Peer for local machine
     // var peer = new Peer({
@@ -256,15 +498,15 @@ $(() => {
     // });
 
 
-    // configuring PeerJSServer on Heroku
-    // Creating a new Peer
+    // // // configuring PeerJSServer on Heroku
+    // // // Creating a new Peer
 
-    var peer = new Peer({
-        secure: true,
-        host: 'the-peer-chat.herokuapp.com',
-        port: 443,
-        path: '/p2pServer',
-    });
+    // var peer = new Peer({
+    //     secure: true,
+    //     host: 'the-peer-chat.herokuapp.com',
+    //     port: 443,
+    //     path: '/p2pServer',
+    // });
 
 
     // When a new Peer is Created
@@ -300,9 +542,6 @@ $(() => {
             refreshUserList(data.list)
         })
 
-        // peer.on('disconnected', function () {
-        // });
-
 
     })
 
@@ -312,7 +551,6 @@ $(() => {
         console.log("Error Type: ", err.type);
         alert("Error Encountered : " + err.type);
     })
-
 
     // Recieving a connection
     peer.on('connection', function (connectionObject) {
@@ -327,11 +565,8 @@ $(() => {
                 // console.log('Received', data);
                 chatList.append(`<li>${data}</li>`);
             });
-
         });
-
     });
-
 
     // Clikcing on the Connect Button
     connectButtton.click(() => {
@@ -388,12 +623,12 @@ $(() => {
             call.close();
         })
 
-        call.on('close', function () {
-            console.log("in call.on(close)");
-            var video = document.getElementById('vChat')
-            video.srcObject = null;
+        // call.on('close', function () {
+        //     console.log("in call.on(close)");
+        //     var video = document.getElementById('vChat')
+        //     video.srcObject = null;
 
-        })
+        // })
 
         call.on('stream', function (stream) {
             // `stream` is the MediaStream of the remote peer.
@@ -404,9 +639,7 @@ $(() => {
             var video = document.getElementById('vChat')
             video.srcObject = stream;
             video.play()
-
         });
-
     });
 
 
@@ -435,17 +668,19 @@ $(() => {
             }
 
             // attach your own stream
-            var video = document.getElementById('vChatMyStream')
-            video.srcObject = stream;
-            video.play()
+            var videoMe = document.getElementById('vChatMyStream')
+            videoMe.srcObject = stream;
+            videoMe.play()
 
 
             vChatend.click(() => {
                 console.log("end Chat milane vale ke liye clickedddd");
                 call.close();
-                var video = document.getElementById('vChatMyStream')
-                video.srcObject = null;
+                var videoMe = document.getElementById('vChatMyStream')
+                videoMe.srcObject = null;
 
+                // tob be added 
+                // videoMe.load();
             })
 
         }, function (err) {
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const ExpressPeerServer = require('peer').ExpressPeerServer;
+
+// const ExpressPeerServer = require('peer').ExpressPeerServer;
+
 const http = require('http');
 const socket = require('socket.io');
 
@@ -23,15 +25,15 @@ server.listen(PORT, () => {
     console.log("p2pChat Running on : http://localhost:3000/p2pChat")
 })
 
-const options = {
-    debug: true
-}
+// const options = {
+//     debug: true
+// }
 
-const peerserver = ExpressPeerServer(server, options);
+// const peerserver = ExpressPeerServer(server, options);
 
 app.use('/', express.static(__dirname + '/scripts'))
 
-app.use('/p2pServer', peerserver);
+// app.use('/p2pServer', peerserver);
 
 app.get('/p2p', (req, res) => {
     res.render('temp.hbs', { users })
@@ -41,11 +43,11 @@ app.get('/p2pChat', (req, res) => {
     res.render('index');
 })
 
-peerserver.on('connection', (client) => {
-    console.log("Client connected : ", client)
-    // users.push(client)
-    // console.log("Users Available : ", users)
-});
+// peerserver.on('connection', (client) => {
+//     console.log("Client connected : ", client)
+//     // users.push(client)
+//     // console.log("Users Available : ", users)
+// });
 
 
 // Old Code ->
@@ -88,7 +90,54 @@ peerserver.on('connection', (client) => {
 // })
 
 
-// New Code from Peerchat Application ->
+// // Revision-1 Code from Peerchat Application ->
+
+// io.on('connection', (socket) => {
+//     console.log("Connection Established :", socket.id)
+
+//     // console.log("Type of socket.id -->", typeof(socket.id)) <= string
+//     // When the connection is made succesfully
+//     socket.emit('connected')
+
+//     socket.on('addUser', (data) => {
+//         console.log(data)
+//         let obj = {
+//             username: data.username,
+//             peerId: data.peerId,
+//             socketId: socket.id     // added sccket.id field in a listOfUsers object.
+//         }
+//         console.log("Object at serevr ->", obj)
+//         listOfUsers.push(obj)
+//         // usersockets[data.user] = socket.id
+//         // // console.log(typeof (usersockets));   console.log(usersockets)
+
+
+//         io.emit('alertAllAboutNewUser', {
+//             list: listOfUsers
+//         })
+//     })
+
+
+//     // When a scket/ Peer is being disconnected
+//     socket.on('disconnect', (reason) => {
+//         console.log('user disconnected, socketID : ', socket.id);
+
+//         // joGyaUskiId = socket.id;
+
+//         listOfUsers = listOfUsers.filter(obj => obj.socketId == socket.id)
+//         console.log(listOfUsers)
+
+//         //    remove user from listOfUsers and the emit alert all
+//         io.emit('alertAllAboutNewUser', {
+//             list: listOfUsers
+//         })
+
+//     });
+
+// })
+
+
+// Revision-2 Code from Peerchat Application ->
 
 
 io.on('connection', (socket) => {
@@ -99,6 +148,9 @@ io.on('connection', (socket) => {
     socket.emit('connected')
 
     socket.on('addUser', (data) => {
+        
+        console.log("inside .on( addUser)");
+
         console.log(data)
         let obj = {
             username: data.username,
@@ -116,8 +168,7 @@ io.on('connection', (socket) => {
         })
     })
 
-
-    // When a scket/ Peer is being disconnected
+    // disconnect code from Chat.IO
     socket.on('disconnect', (reason) => {
         console.log('user disconnected, socketID : ', socket.id);
 
